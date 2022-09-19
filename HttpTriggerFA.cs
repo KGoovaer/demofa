ea62c72eb62f019cec679a015f4581a1257680b1
kgoovaer <koen.goovaerts@ae.be>
2022-09-19 14:41:04 +0200

    add custom message tot queue

--- HttpTriggerFA.cs
+++ HttpTriggerFA.cs
@@ -8,24 +8,34 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Microsoft.Azure.WebJobs.ServiceBus;
+using Azure.Messaging.ServiceBus;
 
 namespace KGoovaer.Function
 {
     public static class HttpTriggerFA
     {
+
+        static ServiceBusClient client;
+        static ServiceBusSender sender;
+
         [FunctionName("HttpTriggerFA")]
-        [return: ServiceBus("demofabusqueue", EntityType.Queue)]  
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
+            var clientOptions = new ServiceBusClientOptions() { TransportType = ServiceBusTransportType.AmqpWebSockets };
+            client = new ServiceBusClient(Environment.GetEnvironmentVariable("AzureWebJobsServiceBus"), clientOptions);
+            sender = client.CreateSender(Environment.GetEnvironmentVariable("Queue"));
+
             string name = req.Query["name"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
+
+            await sender.SendMessageAsync(new ServiceBusMessage(data.ToString()));
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
